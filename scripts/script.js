5cd96280709c14c3c1f7c81414f590c4dc3098d1
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-01-21 19:45:12 +0600

    styled font and color

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'color-primary': '#777',
+        'color-primary-bg': 'rgba(231, 111, 81, 0.10)',
+        'g-start':'#FF8938',
+        'g-end':'#F00'
+      }
+    }
+  }
+}
